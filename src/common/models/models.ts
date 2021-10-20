aabb86fd54090f309404c17d53e15e959e7159fd
Jorge Chable <38798286+iOrchK@users.noreply.github.com>
2021-10-20 18:41:52 -0500

    env vars

--- src/common/models/models.ts
+++ src/common/models/models.ts
@@ -1,11 +1,11 @@
-export const ATRIBUTO = { name: 'atributos' };
-export const CATALOGO = { name: 'catalogos' };
+export const ATRIBUTO = { name: 'ref_atributos' };
+export const CATALOGO = { name: 'ref_catalogos' };
 
-export const CATREGLAS = { name: 'catreglas' };
-export const REGLAS = { name: 'reglas' };
-export const GPOPERACIONES = { name: 'gpoperaciones' };
-export const CATCOMPARECENCIA = { name: 'catcomparecencias' };
-export const OBJETO = { name: 'objetos' };
-export const OPOBJETO = { name: 'opobjetos' };
-export const OPERACION = { name: 'operaciones' };
-export const EXPEDIENTE = { name: 'expedientes' };
+export const CATREGLAS = { name: 'ref_catreglas' };
+export const REGLAS = { name: 'ref_reglas' };
+export const GPOPERACIONES = { name: 'ref_gpoperaciones' };
+export const CATCOMPARECENCIA = { name: 'ref_catcomparecencias' };
+export const OBJETO = { name: 'ref_objetos' };
+export const OPOBJETO = { name: 'ref_opobjetos' };
+export const OPERACION = { name: 'ref_operaciones' };
+export const EXPEDIENTE = { name: 'ref_expedientes' };
